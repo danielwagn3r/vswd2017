a28fc739471567e3f34a4d3b7d3da89a83d39e4a
Daniel Wagner <daniel@wagners.name>
2017-09-20 08:02:31 +0200

    Fixed presentation script.

--- presentation.sh
+++ presentation.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
+
 clear
 ./imgcat logo.png
 read
+
 node part-one.js
+
 clear
 ./imgcat linz.jpg
 read
-node part-two.js
+
 clear
 ./imgcat lauch.jpg
 read
+
 clear
 ./imgcat horn.jpg
 read
+
 node part-two.js
+
 clear
 ./imgcat girls.jpg
 read
+
 clear
 ./imgcat raspberry.jpg
 read
+
 node part-three.js
